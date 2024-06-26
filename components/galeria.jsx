0c7d27108b93ff24670bd4826d4caacd7d64e3c5
Natyguimaraes <santosnath433@gmail.com>
2024-06-26 15:48:32 -0300

    first

--- components/galeria.jsx
+++ components/galeria.jsx
@@ -1,60 +1,48 @@
 import React, { useState } from 'react';
-import "../css/index.css";
 import Menu from "./menu";
 import Imagem1 from "/1.jpeg";
-/*import Imagem2 from "/2.jpeg";
-import Imagem3 from "/3.jpeg";
-import Imagem4 from "/4.jpeg";
-import Imagem5 from "/5.jpeg";
-*/
-function Galeria() {
+import Imagem2 from "/17062023.jpg";
+import Imagem3 from "/23072023.jpg";
+// import Imagem4 from "/4.jpeg";
+// import Imagem5 from "/5.jpeg";
 
+function Galeria() {
     const [secaoAtual, setSecaoAtual] = useState('galeria');
 
     const cliqueSecao = (secao) => {
         setSecaoAtual(secao);
     };
 
+    const images = [
+        { src: Imagem1, alt: 'Descrição da imagem 1', titulo: '06 de agosto de 2023' },
+        { src: Imagem2, alt: 'Descrição da imagem 2', titulo: '17 de junho de 2023' },
+        { src: Imagem3, alt: 'Descrição da imagem 3', titulo: '23 de julho de 2023' },
+        // { src: Imagem4, alt: 'Descrição da imagem 4', titulo: 'Título 4' },
+        // { src: Imagem5, alt: 'Descrição da imagem 5', titulo: 'Título 5' },
+    ];
+
     return (
         <>
-        <div>
-            {secaoAtual === 'galeria' && (
-                <>
-                    <div className="text-intro6">
-                        <h1>Nossa Galeria Virtual</h1>
-                        <div className="imagens">
-                            <div className="img1">
-                            <img src={Imagem1} alt="Descrição da imagem" />
-                            <h2> Título</h2>
+            <div style={{ backgroundColor: '#fff', minHeight: '100vh' }}>
+                {secaoAtual === 'galeria' && (
+                    <>
+                        <div className="text-intro6">
+                            <h1>Nossa Galeria Virtual</h1>
+                            <div className="imagens">
+                                {images.map((image, index) => (
+                                    <div key={index} className="img-container">
+                                        <img src={image.src} alt={image.alt} />
+                                        <h2>{image.titulo}</h2>
+                                    </div>
+                                ))}
                             </div>
-                            </div>
-
-                           {/* <div className="img2">
-                            <img src={Imagem2} alt="Descrição da imagem" />
-                            </div>
-
-                            <div className="img3">
-                            <img src={Imagem3} alt="Descrição da imagem" />
-                            </div>
-                            
-                            <div className="img4">
-                            <img src={Imagem4} alt="Descrição da imagem" />
-                            </div>
-
-                            <div className="img5">
-                            <img src={Imagem5} alt="Descrição da imagem" />
-                            </div>
-            */}
-                        
-
-                       
-                    </div>
-                <button className="buttonMenuVoltar" onClick={() => cliqueSecao('menu')}> Voltar </button></>
-            )}
-
-            <div className='secao'>
-                {secaoAtual === 'menu' && <Menu />}
-            </div>
+                        </div>
+                        <button className="buttonMenuVoltar" onClick={() => cliqueSecao('menu')}>Voltar</button>
+                    </>
+                )}
+                <div className='secao'>
+                    {secaoAtual === 'menu' && <Menu />}
+                </div>
             </div>
         </>
     );
